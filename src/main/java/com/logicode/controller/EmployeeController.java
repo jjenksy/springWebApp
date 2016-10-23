4295f29a8bd7bd8051492b4da9a03bc766d791ac
John <jjenksy@gmail.com>
2016-10-23 12:29:28 -0500

    Added an employee class that uses restful web services to display employee information

---
+++ src/main/java/com/logicode/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.logicode.controller;
+
+import com.logicode.model.Employee;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jjenkins on 10/21/2016.
+ * This controller uses restful services creating a JSON object to allow fo passing of data to the client
+ *
+ */
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    Employee employee = new Employee();
+
+    /**
+     * Restful Get method that produces a JSON to be consumed
+     * @param name
+     * @return the json the can then be consumed for the rest service
+     *
+     * @example http://localhost:8080/greeting/john
+     *
+     */
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/json")
+    public Employee getEmployeeInJSON(@PathVariable String name) {
+
+        employee.setName(name);
+        employee.setEmail(name+"@logicode.com");
+        employee.setBio("A super great full stack developer!!");
+        System.out.println(employee);
+
+        return employee;
+
+    }
+}
