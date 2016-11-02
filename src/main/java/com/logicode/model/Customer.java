d17fc33c09f774855005b5a3b2a5ec097959ee7f
John Jenkins <jjenksy@gmail.com>
2016-11-02 15:40:05 -0500

    Added new databases and controller itterator

--- src/main/java/com/logicode/model/Customer.java
+++ src/main/java/com/logicode/model/Customer.java
@@ -1,4 +1,4 @@
-package com.logicode.model;
+package com.logicode.Model;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
--- src/main/java/com/logicode/model/Employee.java
+++ src/main/java/com/logicode/model/Employee.java
@@ -1,4 +1,4 @@
-package com.logicode.model;
+package com.logicode.Model;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.File;
